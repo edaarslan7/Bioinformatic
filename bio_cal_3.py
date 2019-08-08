026e65d47a08f61a4fa75ec9ef6c6a461846433c
Eda Arslan <34687342+edaarslan7@users.noreply.github.com>
2019-08-08 14:30:50 +0300

    Create bio_cal_3.py

---
+++ bio_cal_3.py
@@ -0,0 +1,17 @@
+#Reverse Complement
+#Find the reverse complement of a DNA string
+def reverse_complement(str1):
+    print("eski değer : ",str1)
+    for i in range(len(str1)):
+        if(str1[i]=='a'):
+            str1 = str1[0:i] + 't' + str1[i+1:len(str1)]
+        elif(str1[i]=="t"):
+             str1 = str1[0:i] + 'a' + str1[i+1:len(str1)]
+        elif(str1[i]=="g"):
+             str1 = str1[0:i] + 'c' + str1[i+1:len(str1)]
+        else:
+             str1 = str1[0:i] + 'g' + str1[i+1:len(str1)]
+    print("son değer : ",str1[::-1]) 
+
+str1 = input("Bir string giriniz: ")
+print(reverse_complement(str1))
