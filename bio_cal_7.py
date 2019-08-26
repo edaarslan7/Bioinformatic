892ca1c06517e6d8c3fd6acf0af76627b7514948
Eda Arslan <34687342+edaarslan7@users.noreply.github.com>
2019-08-26 13:40:20 +0300

    Create bio_cal_7.py

---
+++ bio_cal_7.py
@@ -0,0 +1,18 @@
+#hamming distance problem
+def hamming_distance(s1,s2):
+    if(len(s1)==len(s2)):
+        p = 0
+        diff = 0
+        for i in s1:
+            if(i!=s2[p]):
+                diff += 1
+                p += 1
+            else:
+                p += 1
+        return diff
+    else:
+        print("Esit uzunlukta olmali...")
+        
+str1 = input("Bir DNA parcasi giriniz: ")
+str2 = input("Baska bir tane daha giriniz: ")
+hamming_distance(str1,str2)        
