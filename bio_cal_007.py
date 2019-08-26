c50a92bd28ba4be2b8213a203a39275eb2dfc291
Eda Arslan <34687342+edaarslan7@users.noreply.github.com>
2019-08-26 14:18:42 +0300

    Hanoi towers problem

--- bio_cal_007.py
+++ bio_cal_007.py
@@ -1,9 +1,10 @@
 def Hanoi_towers(n,startPeg,destinationPeg):
     if(n==1):
-        print("diski baslangic peginden hedef pege tasi")
+        print("diski baslangic peginden",startPeg, "hedef pege tasi",destinationPeg)
         return
     transitPeg = 6-startPeg-destinationPeg
     Hanoi_towers(n-1,startPeg,transitPeg)
-    print("disk baslangic peginden hedef pege tasindi")
+    print("disk baslangic peginden",startPeg,  "hedef pege tasindi",destinationPeg)
     Hanoi_towers(n-1,transitPeg,destinationPeg)
     return
+Hanoi_towers(3,2,4)
