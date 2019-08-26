e2653838cdc037ed555aa8a0560d0e7c676f06d6
Eda Arslan <34687342+edaarslan7@users.noreply.github.com>
2019-08-26 13:56:42 +0300

    DNA frequencies

--- bio_cal_002.py
+++ bio_cal_002.py
@@ -2,14 +2,12 @@ def frequent_words(str1):
     a,k=0,3
     liste=[]
     count=[]
-
     while(k<=len(str1)):
         for i in range(a,k):
             str2=str1[a:k]
         a+=1
         k+=1
         liste.append(str2)
-
     for i in liste:
         count.append(liste.count(i))
     return (count)
